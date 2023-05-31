56f9f9920072f2bbb50a42a2a691569c97ae9726
Shyam Singh <111563134+Shyam165@users.noreply.github.com>
2023-05-31 21:30:29 +0530

    Update contact_us.php
    adding some php code for insert the details in database

--- contact_us.php
+++ contact_us.php
@@ -1,3 +1,26 @@
+<?php
+session_start();
+include("db.php");
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+    $fname = $_POST['fname'];
+    $Email = $_POST['Email'];
+    $Subject = $_POST['Subject'];
+    $message = $_POST['message'];
+
+   
+        $query= "insert into Contact (fname, Email, Subject, message) values ('$fname', '$Email', '$Subject', '$message')";
+        $result=mysqli_query($con, $query);
+        if($result){
+        echo "<script type='text/javascript'> alert('Successfully Send Message')</script>";
+        }
+        else {
+  echo "<script type='text/javascript'> alert('Please enter some valid information')</script>";
+        }
+}
+?>
+
+
 <!DOCTYPE html>
 <html>
 
@@ -76,13 +99,13 @@
             <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" style="border: 10px; width: 100%; height: 290px;" ></iframe>
     </div></div>
     <div class="container2">
-     <form action="/action_page.php"><div class="font">
+     <form method="POST"><div class="font">
         <div class="flex">
         <div class="flexx"> <label for="fname">Your Name:</label>
         <br>
         <input type="text" id="fname" class="form" name="fname"placeholder = "Your Name" size="25px"  style="height:40px;font-size:20px;text-align: center;"></div><br>
        <div class="flexx2"> <label for=" E-Mail">E-Mail:</label><br>
-        <input type="text" id="E-Mail" name="E-Mail" placeholder="E-mail"  size="25px"  style="height:40px;font-size:20px; text-align: center;" ></div></div><br>
+        <input type="text" id="Email" name="Email" placeholder="E-mail"  size="25px"  style="height:40px;font-size:20px; text-align: center;" ></div></div><br>
        <div class="mrgin"> <label for=" Subject">Subject:</label><br>
         <input type="text" id="Subject" name="Subject" placeholder="Subject" size="58px"  style="height:40px;font-size:20px;text-align: center;"><br>
         </div><br><div class="flexx3">
